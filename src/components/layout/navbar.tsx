e7bda84074fe8462f87c8a1ec2cafab437b00ee7
Noxyangel <forgotrrr@gmail.com>
2023-03-10 22:08:10 +0300

    text intro

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,20 +2,55 @@ import Logo from "@/components/shared/icons/logo.svg";
 import { NAV_LINKS } from "@/lib/constants";
 import Link from "next/link";
 import { type NextRouter, useRouter } from "next/router";
+import GlowWrap from "../shared/glowwrap";
 
 export default function Navbar() {
     const router: NextRouter = useRouter();
     return (
-        <header className="relative flex h-[6rem] w-full flex-row  items-center justify-between gap-x-16 overflow-hidden xl:mt-10">
-            <Logo className="ml-12 grid  flex-none basis-12 place-items-center " />
-            <div className="relative m-0 hidden  h-0  flex-grow !border-t-transparent p-0 after:absolute after:-right-24 after:left-0 after:top-0 after:z-10  after:m-0 after:block after:h-0 after:w-[calc(100%+6rem)] after:border-b after:border-solid after:border-secondary/25 xl:block xl:after:content-['']" />
-            <nav className=" h-full bg-secondary/5 px-32 backdrop-blur-sm">
-                <ul className="hidden h-full  md:flex md:flex-row md:gap-x-12">
+        <>
+            <header className="relative flex h-[6rem] w-full flex-row  items-center justify-between gap-x-16 overflow-hidden xl:mt-10">
+                <Logo className="ml-12 grid  flex-none basis-12 place-items-center " />
+                <div className="relative m-0 hidden  h-0  flex-grow !border-t-transparent p-0 after:absolute after:-right-24 after:left-0 after:top-0 after:z-10  after:m-0 after:block after:h-0 after:w-[calc(100%+6rem)] after:border-b after:border-solid after:border-secondary/25 xl:block xl:after:content-['']" />
+                {/* Desktop and Tablet navigation menu */}
+                <nav className="hidden h-full bg-secondary/5 px-32 backdrop-blur-sm md:block">
+                    <ul className="hidden h-full  md:flex md:flex-row md:gap-x-12">
+                        {NAV_LINKS.map((item, id) => {
+                            return (
+                                <li
+                                    key={`nav-${item.name}`}
+                                    className={` grid h-full place-items-center  uppercase text-secondary transition-colors `}
+                                >
+                                    <Link
+                                        href={item.url}
+                                        className={`${
+                                            router &&
+                                            router.pathname == item.url
+                                                ? "border-secondary "
+                                                : "border-transparent "
+                                        } text-shadow flex h-full flex-row items-center border-b-[3px] text-center   [&:is(:hover,:focus)]:border-secondary/50`}
+                                    >
+                                        <span className="inline-block pr-2 font-bold tracking-wider md:hidden xl:inline-block">
+                                            {id.toLocaleString("en-US", {
+                                                minimumIntegerDigits: 2,
+                                                useGrouping: false,
+                                            })}
+                                        </span>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </header>
+            {/* Mobile navigation menu */}
+            <nav className="fixed right-0 top-0 z-[9000] h-full bg-secondary/5 backdrop-blur-sm md:hidden">
+                <ul className="flex h-full w-full flex-col md:hidden md:gap-y-12">
                     {NAV_LINKS.map((item, id) => {
                         return (
                             <li
                                 key={`nav-${item.name}`}
-                                className={` grid h-full place-items-center  uppercase text-secondary transition-colors `}
+                                className={`grid w-full place-items-center  uppercase text-secondary transition-colors `}
                             >
                                 <Link
                                     href={item.url}
@@ -23,7 +58,7 @@ export default function Navbar() {
                                         router && router.pathname == item.url
                                             ? "border-secondary "
                                             : "border-transparent "
-                                    } text-shadow border-b-[3px] py-8   text-center [&:is(:hover,:focus)]:border-secondary/50`}
+                                    } text-shadow flex w-full flex-row items-center border-l-[3px] px-12 py-8 text-center   [&:is(:hover,:focus)]:border-secondary/50`}
                                 >
                                     <span className="inline-block pr-2 font-bold tracking-wider md:hidden xl:inline-block">
                                         {id.toLocaleString("en-US", {
@@ -38,6 +73,6 @@ export default function Navbar() {
                     })}
                 </ul>
             </nav>
-        </header>
+        </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,73 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Balancer from "react-wrap-balancer";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 import Explore from "@/lib/buttons/explore";
 
 export default function Home() {
+    const h5Text = "So, you want to travel to";
+    const h1Text = "Space";
+    const longText =
+        "Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!";
 
-    const h5Text="So, you want to travel to"
-    const h1Text="Space"
+    const sentence: Variants = {
+        hidden: {
+            opacity: 1,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.08,
+            },
+        },
+    };
+    const letter: Variants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+        },
+    };
+    const article: Variants = {
+        hidden: {
+            opacity: 1,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.005,
+                delay: 10,
+                when: "beforeChildren",
+            },
+        },
+    };
+    const title: Variants = {
+        hidden: {
+            mask: "linear-gradient(to left, transparent 40%,black 50%, black 60%,transparent 80%)",
+            maskPosition: "80% 0%",
+            maskSize: "800%",
+            WebkitMask:
+                "linear-gradient(to left, transparent 40%,black 50%, black 60%,transparent 80%)",
+            WebkitMaskPosition: "80% 0%",
+            WebkitMaskSize: "800%",
+            opacity: 1,
+        },
+        visible: {
+            opacity: 1,
 
-    const sentence = {
-        hidden:{
-            opacity:1
-        }
-    }
+            maskPosition: "45% 0%",
+
+            WebkitMaskPosition: "45% 0%",
+
+            transition: {
+                duration: 10,
+                type: "tween",
+                ease: "easeOut",
+                delay: 2,
+                // times: [0, 0.2, 0.3, 1],
+            },
+        },
+    };
     return (
         <>
             <Head>
@@ -22,23 +76,52 @@ export default function Home() {
             </Head>
 
             <div className="container grid h-full grid-cols-1 grid-rows-home place-items-end justify-items-start overflow-y-clip px-6 lg:px-24 xl:grid-cols-2 xl:px-40">
-                <article className="text-center xl:text-left">
-                    <h5 className="tracking-widest text-accent">
-                        So, you want to travel to
-                    </h5>
-                    <h1 className="spicy mb-[0.15em] w-full bg-conic bg-[size:800%+800%] bg-no-repeat">
-                        Space
-                    </h1>
-                    <p className="text-shadow max-w-[46ch] text-accent">
+                <motion.article
+                    className="text-center xl:text-left"
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <motion.h5
+                        className="tracking-widest text-accent"
+                        variants={sentence}
+                    >
+                        {h5Text &&
+                            h5Text.split("").map((char, index) => {
+                                return (
+                                    <motion.span
+                                        key={char + "-" + index}
+                                        variants={letter}
+                                    >
+                                        {char}
+                                    </motion.span>
+                                );
+                            })}
+                    </motion.h5>
+                    <motion.h1
+                        variants={title}
+                        className="spicy linear-mask relative mb-[0.15em] w-full bg-conic bg-[size:800%+800%] bg-no-repeat"
+                    >
+                        {h1Text}
+                    </motion.h1>
+                    <motion.p
+                        className="text-shadow max-w-[46ch] text-accent"
+                        variants={article}
+                    >
                         <Balancer ratio={0.5}>
-                            Let’s face it; if you want to go to space, you might
-                            as well genuinely go to outer space and not hover
-                            kind of on the edge of it. Well sit back, and relax
-                            because we’ll give you a truly out of this world
-                            experience!
+                            {longText &&
+                                longText.split("").map((char, index) => {
+                                    return (
+                                        <motion.span
+                                            key={char + "-" + index}
+                                            variants={letter}
+                                        >
+                                            {char}
+                                        </motion.span>
+                                    );
+                                })}
                         </Balancer>
-                    </p>
-                </article>
+                    </motion.p>
+                </motion.article>
 
                 <div className="relative h-min w-min justify-self-end ">
                     <Explore />
